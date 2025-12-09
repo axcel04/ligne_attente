945257990e63ea056549876faa84ce41acfaf3fe
Yves <yvart.pro@gmail.com>
2025-12-09 10:37:43 +0200

    display ticket

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,24 +1,40 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import Header from "../Component/Header";
 import Footer from "../Component/Footer";
 import Footer1 from "../Component/Footer1";
 import { Bell } from "lucide-react";
+import { useApi } from "../api";
+import { useAppContext } from "../context/AppContext";
 
 export default function Home() {
-  const [ticket, setTicket] = useState({
-    number: "A12",
-    service: "Consultation Général",
-    position: 1, // Si position = 1, notification izahita igaragara
-    stutus: "En attente",
-  });
+  const { API_URL } = useAppContext();
+  const api = useApi()
+  const [ticket, setTicket] = useState({});
 
   const [notify, setNotify] = useState(false);
+  const [position, setPosition] = useState(null);
 
   useEffect(() => {
-    if (ticket.position <= 1) {
-      setNotify(true); // Notification automatique
-    }
-  }, [ticket]);
+    api.get(`${API_URL}/ticket`)
+      .then((response) => {
+        // get latest 
+        const latestTicket = response.data.slice(-1)[0];
+        // calculate position base on other t with status en_attente and created before latestTicket
+        const pos = response.data.filter(t => t.status === 'en_attente' && new Date(t.created_at) < new Date(latestTicket.created_at)).length + 1;
+        latestTicket.pos = pos;
+        // if position is 1 set notify to true after 5 seconds
+        if(pos === 1){
+          setTimeout(() => {
+            setNotify(true);
+          }, 5000);
+        }
+        setPosition(pos);
+        setTicket(latestTicket);
+      })
+      .catch((error) => {
+        console.error("Erreur lors de la récupération du ticket :", error);
+      });
+  },[])
 
   return (
     <>
@@ -32,10 +48,10 @@ export default function Home() {
           </h2>
 
           <div className="relative p-6 bg-blue-50 rounded-xl space-y-4">
-            <span className="absolute top-6 right-2 px-2 py-1 text-yellow-600 bg-yellow-100 rounded-full">{ticket.stutus}</span>
+            <span className="absolute top-6 right-2 px-2 py-1 text-yellow-600 bg-yellow-100 rounded-full">{ticket.status}</span>
             {/* Numéro du ticket */}
             <div className="text-center">
-              <h3 className="text-4xl font-bold text-blue-700">{ticket.number}</h3>
+              <h3 className="text-4xl font-bold text-blue-700">{`A0${ticket.id}`}</h3>
               <p className="text-gray-600 mt-1 text-sm">Numéro de ticket</p>
             </div>
 
@@ -46,7 +62,7 @@ export default function Home() {
 
             {/* Position */}
             <div className="text-center text-gray-700 font-semibold">
-              Position dans la file : {ticket.position}
+              Position dans la file : {position !== null ? position : '...'}
             </div>
 
             {/* Notification */}
